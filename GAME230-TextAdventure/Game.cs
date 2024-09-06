672b35affb2397eed016199d58f7e1913332f5c3
Cristian Melgoza <91515108+MOONtyzoo@users.noreply.github.com>
2024-09-05 18:43:25 -0700

    Create Program and Game classes

---
+++ GAME230-TextAdventure/Game.cs
@@ -0,0 +1,20 @@
+﻿namespace GAME230_TextAdventure;
+
+public static class Game
+{
+    private static bool _isGameOver = false;
+    public static void Play()
+    {
+        Console.WriteLine("Playing the game");
+
+        while (_isGameOver == false)
+        {
+            Console.Write("> ");
+            string input = Console.ReadLine();
+            if (input == "exit")
+            {
+                _isGameOver = true;
+            }
+        }
+    }
+}
--- GAME230-TextAdventure/Program.cs
+++ GAME230-TextAdventure/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace GAME230_TextAdventure;
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Game.Play();
+    }
+}
